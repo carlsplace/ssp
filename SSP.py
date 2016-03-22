9cf06a6bdf31a5fc3039ce66c15eaec4480d77e2
Cal <chang.1123@outlook.com>
2016-03-22 13:59:10 +0800

    not complete yet

--- SSP.py
+++ SSP.py
@@ -16,7 +16,7 @@ def getTransMatrix(DG):
 	P = np.array(P)
 	return P
 
-def calcPi3(node, phi, pi):
+def calcPi3(node, phi, pi, P):
 	# r is the reset probability vector, pi3 is an important vertor for later use
 	node_weight = {}
 	r_temp =[]
@@ -29,41 +29,41 @@ def calcPi3(node, phi, pi):
 	pi3 = d * np.dot(P.T, pi.T) + (1 - d) * r - pi
 	return pi3
 
-def calcGradientPi(pi, r, P, B, miu):
+def calcGradientPi(pi, P, B, miu):
 	P1 = d * P - np.identity(nv)
 	g_pi = (1 - alpha) * np.dot(P1, pi3.T).T - alpha/2 * np.dot(B.T, miu.T)
 	return g_pi
 
 def get_W(i, j, k):
 	ij = 10 * (i + 1) + j + 1
-	if ij in x:
-		result = (x[ij][k] * sumjk[i] - sumk[ij] * sumj[i][k]) / (sumjk[i] * sumjk[i])
+	if ij in edge:
+		result = (edge[ij][k] * sumjk[i] - sumk[ij] * sumj[i][k]) / (sumjk[i] * sumjk[i])
 	else:
 		result = 0
 	return result
 
-def calcGradientOmega(x, omega):
+def calcGradientOmega(edge, omega):
 	# complicated, holy shit...
 	sumjk = []
 	for i in range(1, nv+1):
 		sumjk_temp = 0
-		for key in x:
+		for key in edge:
 			if key/10 == i:
-				sumjk_temp += np.dot(x[key], omega.T)
+				sumjk_temp += np.dot(edge[key], omega.T)
 		sumjk.append(sumjk_temp)
 
 	sumk = {}
-	for key in x:
-		sumk[key] = np.dot(x[key], omega.T)
+	for key in edge:
+		sumk[key] = np.dot(edge[key], omega.T)
 
-	sumj = [([0] * nvf) for i in range(n)]
+	sumj = [([0] * nvf) for i in range(nv)]
 	sumj_temp = 0
 	for k in range(0, nvf):
-		for i in range(1, n+1):
+		for i in range(1, nv+1):
 			sumj_temp = 0
-			for key in x:
+			for key in edge:
 				if key/10 == i:
-					sumj_temp += x[key][k]
+					sumj_temp += edge[key][k]
 					sumj[i-1][k] = sumj_temp
 
 	W_temp = [([0] * nvf) for i in range(nv * nv)]
@@ -75,10 +75,10 @@ def calcGradientOmega(x, omega):
 	g_omega = (1 - alpha) * np.dot((np.kron(pi3, pi)), W)
 	return g_omega
 
-def calcGradientPhi(pi3, y):
+def calcGradientPhi(pi3, node):
 	R_temp = []
-	for key in y:
-		R_temp.append(y[key])
+	for key in node:
+		R_temp.append(node[key])
 	R = np.array(R_temp)
 	g_phi = (1 - alpha) * (1 - d) * np.dot(pi3, R)
 	return g_phi
@@ -90,3 +90,46 @@ def calcG(pi3, B, miu):
 	return G
 
 # global variable: alpha, d, nv(num_of_nodes), nvf(num_of_nodefeatures)
+d = 0.85
+alpha = 0.5
+step_size = 0.1
+iteration = 0
+
+B = np.array([[0, 1, 0, 0, -1], [0, -1, 1, 0, 0]])
+miu = np.array([0.8, 0.2])
+
+# features of edges
+edge = {
+12 : np.array([5, 7]),
+13 : np.array([4, 5]),
+23 : np.array([6, 4]),
+42 : np.array([7, 4]),
+34 : np.array([3, 7]),
+41 : np.array([7, 6]),
+35 : np.array([4, 6]),
+45 : np.array([5, 4]),
+52 : np.array([6, 6]),
+}
+omega = np.array([0.6, 0.4])
+l = len(omega) 
+# features of nodes
+node = {
+1 : np.array([2, 4, 3]),
+2 : np.array([2, 4, 5]),
+3 : np.array([3, 3, 4]),
+4 : np.array([3, 4, 6]),
+5 : np.array([3, 4, 2]),
+}
+phi = np.array([0.4, 0.3, 0.3])
+
+nv = len(node) # #nodes
+nvf = len(phi) # #node features
+pi = np.full(nv, 1.0/nv)
+
+DG = createDirectedGraph(edge, omega)
+P = getTransMatrix(DG)
+pi3 = calcPi3(node, phi, pi, P)
+G0 = calcG(pi3, B, miu)
+g_phi = calcGradientPi(pi, P, B, miu)
+g_omega =calcGradientOmega(edge, omega)
+g_phi = calcGradientPhi(pi3, node)
